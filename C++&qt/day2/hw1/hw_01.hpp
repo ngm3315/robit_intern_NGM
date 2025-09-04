a1ffaac0f07f2eefc0c964c24305ee390bb9b57e
Ki Moon Noh <ngm3315@naver.com>
2025-09-04 13:01:23 +0900

    Update hw_01.hpp

--- C++&qt/day2/hw1/hw_01.hpp
+++ C++&qt/day2/hw1/hw_01.hpp
@@ -1,13 +1,13 @@
 class ArrayStats {
 private:
-    int* arr;
+    int* arr;   // 정수 배열로 변경
     int size;
 public:
-    ArrayStats(int n);
-    ~ArrayStats();
-    void input();
-    int getMax();
-    int getMin();
-    int getSum();
-    double getAverage();
+    ArrayStats(int n);    // 동적 할당
+    ~ArrayStats();        // 메모리 해제
+    void input();         // 입력 (문자열 검사 후 정수 변환)
+    int getMax();         // 최대값
+    int getMin();         // 최소값
+    int getSum();         // 합
+    double getAverage();  // 평균
 };
